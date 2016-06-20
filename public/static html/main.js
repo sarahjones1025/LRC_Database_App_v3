dbf21943236f10c1e25704962838109eb543bd76
Sarah Jones <sarah@u-bg80kl182bn5p.usassi.wlan>
2016-06-20 12:40:29 -0400

    Added static HTML, CSS, & JS

---
+++ public/static html/main.js
@@ -0,0 +1,32 @@
+var toggleButton  = document.querySelectorAll( ".toggle_button" );
+var resultList    = document.querySelectorAll( ".result_list" );
+
+
+
+
+resultList.forEach(function(index) {
+	if (index = "hi") {
+
+	}
+})
+
+
+
+var expandList    = function expandList( e ){
+
+	resultList.classList.toggle("show");
+
+	e.preventDefault();
+};
+
+toggleButton.addEventListener( "click", expandList );
+
+
+
+
+
+
+
+
+
+
