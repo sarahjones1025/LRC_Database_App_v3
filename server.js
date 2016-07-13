d7db8d68bffe1ac927cc482c4e96557806c45e0e
Daniel_F <Daniel_F@ETBAdmins-MacBook-Pro-5.local>
2016-07-13 13:38:32 -0400

    updated project structure

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-// making an instance of express
 var app = express();
-// app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 
-app.get('/', function (req, res) {
-	res.send('Working');
-});
+	app.get('/',function(req,res){
+        res.render('index.html');
+     });
+	app.get('/index.html',function(req,res){
+        res.render('index.html');
+     });
+     app.get('/ags.html',function(req,res){
+        res.render('ags.html');
+    });
+     app.get('/fms.html',function(req,res){
+        res.render('fms.html');
+    });
+     app.get('/lesson.html',function(req,res){
+        res.render('lesson.html');
+    });
+     app.get('/ncoa.html',function(req,res){
+        res.render('ncoa.html');
+    });
 
-app.listen(process.env.PORT || 3000, function () {
-	console.log('Server is Working');
+     app.get('/data.json',function(req,res) {
+        res.send('data.json');
+     });
+
+var server = app.listen(3000, function() {
+	console.log("We have started our server on port 3000");
 });
