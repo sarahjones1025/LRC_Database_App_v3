b4729b9f132de981721539dcca5762d950562014
Daniel_F <Daniel_F@ETBAdmins-MacBook-Pro-5.local>
2016-07-22 13:38:20 -0400

    pulling data, generating lists

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,7 @@
 var contents      = $( ".contents" );
 var toggleButton  = $( ".toggle_button" );
 var lessonResults = $(".lesson_results");
+var lessonList = $(".lesson_list");
 
 //Clears all options (unchecks checkboxes)
 $('#clearFilter').click(function(e){
@@ -40,13 +41,14 @@ $(document.body).on('click', function (e){
 });
 
 
-
  $(document).ready(function(){
     $("input:checkbox").click(function(){
         if ($(this).is(':checked'))
         {
             $(".instructions").html("Refine your results: (#) Lessons found");
-            lessonResults.append("<li>blahblablah</li>");
+            var course = $(this).parent().text();
+            populateResults(course);
+
         }
         else
         {
@@ -59,22 +61,119 @@ $(document.body).on('click', function (e){
 //currently the clear all criterion does not change the text back
 
 
+
+
+
+
+// var x = new XMLHttpRequest();
+// x.open('GET','data.js', true);
+// x.onreadystatechange = function(){
+//     if(x.readyState == 4){
+//         var dataArray = [];
+//         var data = JSON.parse(x.responseText);
+//         SaveDataToLocalStorage(x.responseText);
+//     }
+// };
+// x.send();
+
+
 var x = new XMLHttpRequest();
 x.open('GET','data.js', true);
 x.onreadystatechange = function(){
     if(x.readyState == 4){
-        console.log('boom');
-        var data = JSON.parse(x.responseText);
-        callback(data[3]['Core Competencies (Name)']);
+        SaveDataToLocalStorage(x.responseText);
     }
 };
 x.send();
 
-function callback(resp){
-    console.log(resp);
+
+
+
+
+
+function SaveDataToLocalStorage(data)
+{
+    var a = [];
+    // Parse the serialized data back into an aray of objects
+    // Push the new data (whether it be an object or anything else) onto the array
+    a.push(data);
+    // Re-serialize the array back into a string and store it in localStorage
+    localStorage.setItem('session', a);
 }
 
 
+// When you click on a checkbox, every single 'lesson', that has that
+// 'course' related to it, will populate a list item:
+// (in addition to what lesson, corresponding info i.e.)
+
+
+
+// this function loops over the localStorage and populates list items when
+// you click on a checkbox:
+
+function populateResults (course) {
+
+    // parse the localStorage data:
+    var data = JSON.parse(localStorage.session);
+    // loop over all the data:
+    data.forEach(function (obj) {
+    // if the current objects Course property is equal to
+    // the course we passed in to populate Results:
+      if (obj.Course === course) {
+
+    var testUL = $("<ul></ul").prop('class', 'testLiClass');
+
+    // Create a list item
+    // Set list items html equal to the obj's LessonName:
+    // Append the liste item to the lessonResults ul:
+
+    // $("<li/>").appendTo(lessonList).html(obj.LessonName); 
+
+    $("<li/>").appendTo(testUL).html(obj.LessonName);  
+    $("<li/>").appendTo(testUL).html(obj.Course);  
+    $("<li/>").appendTo(testUL).html(obj.IndividualTask);  
+    $("<li/>").appendTo(testUL).html(obj.CollectiveTask);  
+
+    lessonList.append(testUL);
+
+        } 
+    });
+}
+
+
+
+
+
+
+
+// function setStorgage (resp) {
+//     var data = JSON.parse(resp);
+//     data.forEach(function(obj) {
+//         window.localStorage.setItem('Course', obj.Course);
+//     });
+// }
+
+// function callback(resp){
+//     console.log(resp);
+// }
+
+
+
+// new request 
+
+// $.ajax({
+//     method: "GET",
+//     url: 'data.js',
+//     dataType: 'JSONp',
+//     success: function(data, status) {
+//         $.each(data, function(key, value){
+//             conosle.log(data);             
+//         });
+//     },
+//     error: function() {
+//         console.log('we got a problem');
+//     }
+// });
 
 
 
